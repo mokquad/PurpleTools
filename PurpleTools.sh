8f56c5975449a302da11db9981ba6259d49a858d
marktarasov <mokquad@gmail.com>
2025-07-30 22:04:56 +0900

    Add files via upload

--- PurpleTools.sh
+++ PurpleTools.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 echo Welcome to PurpleTools! 
 echo Please select an option:
-echo 1. Install
-echo 2. Information & Credits
+echo 1. Install 
+echo 2. Information, requirements & credits
 read -r option
 if [ "$option" = "1" ] ; then
   echo
   echo "Making a backup of SystemVersion.plist..."
   cd /System/Library/CoreServices/
   cp SystemVersion.plist /System/Library/CoreServices/SystemVersion.plist.bak
-  echo "Done!"
-  echo
-  echo "Installing plutil..."
-  apt-get install com.bingner.plutil
   echo "Done!"
   echo
   echo "Modifying ProductType..."
@@ -24,25 +20,29 @@ if [ "$option" = "1" ] ; then
   echo "Done!"
   echo
   echo "Creating folders..."
-  apt -y --allow-unauthenticated install wget
   mkdir /private/var/PurpleTools
   mkdir /AppleInternal
   mkdir /System/Library/Perl
   echo "Done!"
   cd /private/var/PurpleTools
-   echo "Downloading and installing..."
+  echo "Downloading and installing..."
   wget https://archive.org/download/pisyapopa/PurpleTools.zip --no-check-certificate
   unzip PurpleTools.zip
   mv /private/var/PurpleTools/AppleInternal/ /AppleInternal/
+  rm -rf /private/var/PurpleTools/AppleInternal
   mv /private/var/PurpleTools/PrivateFrameworks/ /System/Library/PrivateFrameworks/
+  rm -rf /private/var/PurpleTools/PrivateFrameworks
   mv /private/var/PurpleTools/Perl/ /System/Library/Perl/
+  rm -rf /private/var/PurpleTools/Perl
   echo "Done!"
   echo
   echo "The installation process has finished successfully."
   echo "Respringing your iDevice..."
-  ldrestart
+  killall SpringBoard
  
 elif [ "$option" = "2" ] ; then
-  echo Made by: Internal Store, Mokquad.
   echo PurpleTools version: 1.0
+  echo To get everything done on your device, you need to have:
+  echo Tweaks: plutil, wget, zip, unzip. 
+  echo Repos: https://apt.procurs.us/, main Cydia repo.
 fi
